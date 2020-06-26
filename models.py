e190e98b06acb57717ba6b85093c758de6577962
JAINISHD13 <jainishdabhi@gmail.com>
2020-06-26 14:10:30 -0700

    After getting the final value in Django

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class registerUser(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email_id = models.CharField(primary_key=True,max_length=100)
+    password = models.CharField(max_length=100)
+    confirmPassowrd = models.CharField(max_length=100)
--- views.py
+++ views.py
@@ -1,22 +1,46 @@
 import simplejson as simplejson
-from django.shortcuts import render, render_to_response
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-import json
-from django.views.decorators.csrf import requires_csrf_token, csrf_exempt
+from django.core.serializers import json
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http import JsonResponse
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_protect, csrf_exempt
+
+from .models import registerUser
 
 def index(request):
     return render(request,'index.html',{})
 
 def registerUser(request):
-    context ={}
-    return render(request,'register.html',context)
+    if request.method == 'POST':
+        register1_data = request.POST.dict()
+        firstname = register1_data.get('inputFirstName')
+        lastname = register1_data.get('inputLastName')
+        email_id = register1_data.get('inputEmailAddress')
+        password = register1_data.get('inputPassword')
+        confirmPassowrd = register1_data.get('inputConfirmPassword')
+        print(firstname,lastname,email_id,password)
+        messages.success(request,f'Account created for {firstname}!')
+        return redirect('loginUser')
+    else:
+        return render(request,"register.html")
 
 def loginUser(request):
     context ={}
     return render(request,'login.html',context)
 
+@csrf_exempt
+def getModelAttribute(request):
+    if request.method == "POST" and request.is_ajax():
+        arry1D = request.POST.get('arry1D')
+        count = request.POST.get('count')
+        print(arry1D,count)
+        return JsonResponse({'arry1D':arry1D})
+
 def hello(request):
     return HttpResponse('Hello World!')
 
 def home(request):
-    return render_to_response('response.html', {'variable': 'world'})
+    return render(request,'response.html', {'variable': 'world'})
